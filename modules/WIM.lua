e9a5f8f1d4e3b1c2cf70a618422f481ea449064e
Jon S Akhtar <sylvanaar@mindspring.com>
2008-10-14 11:44:06 +0000

    Fix to fading module so it controls the right fading parameter. WIM compat color value passing.

--- modules/WIM.lua
+++ modules/WIM.lua
@@ -1,7 +1,7 @@
 tinsert(Prat.EnableTasks, function(self) 
 	-- register formatting with WIM.
 	if(_G.WIM and _G.WIM.RegisterMessageFormatting) then
-	   _G.WIM.RegisterMessageFormatting("Prat", Prat.Format);
+	   _G.WIM.RegisterMessageFormatting("Prat", function(smf, event, ...) return Prat.Format(smf, event, _G.WIM.nextColor, ...) end )
 	end
 end )
 
