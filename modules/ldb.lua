4b2c87ad5d13721cdf7c3771bffee098d53f8f1b
Jon S Akhtar <sylvanaar@mindspring.com>
2008-10-25 23:53:50 +0000

    toc and ldb tweaks

--- modules/ldb.lua
+++ modules/ldb.lua
@@ -2,6 +2,7 @@ tinsert(Prat.EnableTasks, function(self)
 	if not LibStub:GetLibrary("LibDataBroker-1.1", true) then return end
 	LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Prat", {
 		type = "launcher",
+		text = "Prat |cff8080ff3.0|r",
 		icon = "Interface\\Addons\\"..Prat.FolderLocation.."\\textures\\chat-bubble",
 		OnClick = function(frame, button)
 			LibStub("AceConfigDialog-3.0"):Open("Prat")
