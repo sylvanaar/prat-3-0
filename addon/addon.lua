6c63efc9952dd8dcb388c7a6ca0d2ccfff645d29
sylvanaar <sylvanaar@5c0c6b0c-531f-4792-90f6-9befda5a6eed>
2010-05-20 21:47:15 +0000

    conditionally remove modules which cause errors on the ptr

--- addon/addon.lua
+++ addon/addon.lua
@@ -77,7 +77,7 @@ setfenv(1, Prat)
 
 NEW_CHATFILTERS = select(4, _G.GetBuildInfo()) >= 30100
 CHAT_PLAYER_GUIDS = select(4, _G.GetBuildInfo()) >= 30200
-
+BN_CHAT = (GetBuildInfo() == "3.3.5") or (GetBuildInfo() == "0.3.5")
 
 if not _G.GetDifficultyColor then _G.GetDifficultyColor = _G.GetQuestDifficultyColor end
 
