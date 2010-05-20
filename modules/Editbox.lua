40ca3747cc40894207352891f488fbfaaab5d0a1
Jon S Akhtar <sylvanaar@mindspring.com>
2010-05-20 21:47:15 +0000

    conditionally remove modules which cause errors on the ptr

--- modules/Editbox.lua
+++ modules/Editbox.lua
@@ -1,5 +1,7 @@
 -- This is the editbox module from Chatter by Antiarc
 
+if Prat.BN_CHAT then return end -- Removed in 3.3.5 
+
 Prat:AddModuleToLoad(function() 
 
 local PRAT_MODULE = Prat:RequestModuleName("Editbox")
