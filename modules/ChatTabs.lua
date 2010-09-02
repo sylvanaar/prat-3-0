ee2753f27d4f90e2c124feff72f85e57c267da6e
Jon S Akhtar <sylvanaar@mindspring.com>
2010-09-02 21:44:39 +0000

    set .oldAlpha explicitly to prevent an error in FCF

--- modules/ChatTabs.lua
+++ modules/ChatTabs.lua
@@ -279,7 +279,7 @@ function module:UpdateAllTabs()
             
             -- Prevent an error in FloatingChatFrame FCF_FadeOutChatFrame() (blizz bug)
             chatTab:SetAlpha(chatTab:GetAlpha() or 0)
-            v:SetAlpha(v:GetAlpha() or 0)
+            v.oldAlpha=v:GetAlpha() or 0
 
             FCF_FadeOutChatFrame(v)
         end
