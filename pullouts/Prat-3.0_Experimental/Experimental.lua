30b90c711edf2ac8b5aaae63fbf1e292652d516b
sylvanaar <sylvanaar@localhost>
2010-12-29 12:40:27 -0500

    save the experimental prototype work

--- pullouts/Prat-3.0_Experimental/Experimental.lua
+++ pullouts/Prat-3.0_Experimental/Experimental.lua
@@ -125,9 +125,107 @@ function PE:AddLines(lines, ...)
   end
 end
 
---[[------------------------------------------------
-	Core Functions
-------------------------------------------------]]--
+local MAX_SCRAPE_TIME = 30
+local scrapelines = {}
+
+
+SLASH_FIND1 = "/find"
+SlashCmdList["FIND"] = function(msg) PE:TS(msg) end
+
+SLASH_FINDALL1 = "/findall"
+SlashCmdList["FINDALL"] = function(msg) PE:TS(msg, true) end
+
+local foundlines = {}
+
+function PE:TS(word, all, frame, noshow)
+
+
+    self.longstr = ""
+    self.str = ""
+
+    if frame == nil then
+        frame = ChatFrame1
+    end
+
+
+    if not word then return end
+
+    if #word <= 1 then
+        frame:ScrollToBottom()
+        Prat:Print("Search term is too short")
+        return
+    end    
+
+    if frame:GetNumMessages() == 0 then return end
+
+
+    local starttime = time()
+    local runtime = 0
+
+    if not all and self.lastsearch == word then
+        frame:PageUp()
+    end
+
+    if all then
+        frame:ScrollToBottom()
+    end
+
+    self.lastsearch = word
+
+    repeat
+        self:DoCopyChat(frame, nil, true)
+
+        for i,v in ipairs(scrapelines) do
+            if v:find(word) then
+                --Prat:Print(("Found line %d scroll %d"):format(frame:GetCurrentLine(), frame:GetCurrentScroll()))
+                if all then
+                    table.insert(foundlines, v)
+                else
+                    return
+                end
+            end
+        end
+
+        frame:PageUp()
+        runtime = time() - starttime
+        if runtime >= MAX_SCRAPE_TIME then
+            Prat:Print("Frame scraping timeout exceeded, results will be incomplete.")
+            break;
+        end
+        
+    until frame:AtTop() or runtime >= MAX_SCRAPE_TIME
+
+    self.lastsearch = nil
+
+    frame:ScrollToBottom()
+
+    if all and  #foundlines > 0 then
+        Prat:Print("Find Results:")
+        
+        for i,v in ipairs(foundlines) do
+            Prat:Print(v)
+        end
+    else
+        Prat:Print("Not Found")
+    end
+
+    wipe(foundlines)
+end
+
+function PE:DoCopyChat(frame, oldsize, noshow)
+    wipe(scrapelines)
+
+    self:AddLines(scrapelines, frame:GetRegions())
+end
+
+function PE:AddLines(lines, ...)
+  for i=select("#", ...),1,-1 do
+    local x = select(i, ...)
+    if x:GetObjectType() == "FontString" and not x:GetName() then
+        table.insert(lines, x:GetText())
+    end
+  end
+end
 
 
   return
