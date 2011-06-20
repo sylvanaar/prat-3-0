b49c1a8912c92a7bf723c4d0e31a32051f483e5a
sylvanaar <sylvanaar@mindspring.com>
2011-06-20 18:35:44 -0400

    autocompletion of table fields too

--- debug/globalcomplete.lua
+++ debug/globalcomplete.lua
@@ -25,11 +25,12 @@ function _M:EnableGlobalCompletions(overrideName, maxResults, prematches)
   self:ReloadGlobals()
 
   self:GlobalTabComplete()
+
+  self:FieldTabComplete()
 end
 
 function _M:GlobalTabComplete()
   if not AceTab:IsTabCompletionRegistered(tabcompleteName) then
-    local foundCache = {}
     AceTab:RegisterTabCompletion(tabcompleteName, self.preMatches,
       function(t, text, pos)
         if (text:trim():len() < 1) then return nil end
@@ -44,9 +45,48 @@ function _M:GlobalTabComplete()
   end
 end
 
+function _M:FieldTabComplete()
+  if not AceTab:IsTabCompletionRegistered(tabcompleteName .. "-fields") then
+    AceTab:RegisterTabCompletion(tabcompleteName .. "-fields", " .+",
+      function(t, text, pos)
+        local lastDot = 0
+        for m in text:gmatch("()%.") do lastDot = m end
+        local s = text:find("%s")
+
+        local m, fs = text:sub(s + 1, lastDot - 1), text:sub(lastDot + 1, -1)
+        fs = fs or ""
+        Prat:PrintLiteral(t, text, pos, m, fs)
+
+
+        if type(m) == "string" then
+          local tmp = loadstring("return " .. m)()
+          if type(tmp) == "table" then
+            for k in pairs(tmp) do
+              if fs:len() > 0 then
+                if k:lower():find(fs, 1, true) == 1 then
+                  t[#t + 1] = m .. "." .. k
+                end
+              else
+                t[#t + 1] = m .. "." .. k
+              end
+            end
+
+            return t
+          end
+        end
+      end,
+      nil,
+      nil,
+      nil,
+      nil)
+  end
+end
+
+
 function _M:GetResultCompletions(u, cands, ...)
   local candcount = 0
-  for k, v in pairs(cands) do candcount = candcount + 1 end
+  for k, v in pairs(cands) do candcount = candcount + 1
+  end
   if candcount <= self.maxResults then
     local text
     for key, cand in pairs(cands) do
@@ -66,6 +106,7 @@ end
 
 function _M:ReloadGlobals()
   wipe(self.globalKeys)
-  for k in pairs(_G) do self.globalKeys[#self.globalKeys + 1] = k end
+  for k in pairs(_G) do self.globalKeys[#self.globalKeys + 1] = k
+  end
   table.sort(self.globalKeys)
 end
