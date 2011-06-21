29ec632601a28879973b54b9a022985c308bfdea
Jon S Akhtar <sylvanaar@mindspring.com>
2011-06-21 02:33:40 -0400

    support :, and only display a list of functions. for widgets the : is the only way to display functions, . just shows fields due to the functions being in the metatable.

--- debug/globalcomplete.lua
+++ debug/globalcomplete.lua
@@ -11,7 +11,7 @@
 --
 -------------------------------------------------------------------------------
 local name, _M = ...
-local tabcompleteName
+
 local pairs, ipairs, _G, table, LibStub, wipe, type, loadstring =
       pairs, ipairs, _G, table, LibStub, wipe, type, loadstring
 
@@ -27,11 +27,11 @@ if not AceTab then
 end
 
 function _M:EnableGlobalCompletions(overrideName, maxResults, prematches)
-  tabcompleteName = overrideName or (name .. "-debug-globals")
+  self.tabcompleteName = overrideName or (name .. "-debug-globals")
 
   self.maxResults = maxResults or 25
   self.globalKeys = {}
-  self.preMatches = prematches or { "/print ", "/dump " }
+  self.preMatches = prematches or { "/print%s+", "/dump%s+" }
 
   self:ReloadGlobals()
 
@@ -41,14 +41,14 @@ function _M:EnableGlobalCompletions(overrideName, maxResults, prematches)
 end
 
 function _M:GlobalTabComplete()
-  if not AceTab:IsTabCompletionRegistered(tabcompleteName) then
-    AceTab:RegisterTabCompletion(tabcompleteName, self.preMatches,
+  if not AceTab:IsTabCompletionRegistered(self.tabcompleteName) then
+    AceTab:RegisterTabCompletion(self.tabcompleteName, self.preMatches,
       function(t, text, pos)
         if (text:trim():len() < 1) then return nil end
         return self:GetPrefilteredCompletions(t, text, pos)
       end,
       function(u, cands, ...) -- usagefunc
-        self:GetResultCompletions(u, cands, ...)
+        return self:GetResultCompletions(u, cands, ...)
       end,
       nil, -- listenframes
       nil, -- postfunc
@@ -56,32 +56,52 @@ function _M:GlobalTabComplete()
   end
 end
 
+local function dbg(...) Prat:PrintLiteral(...) end
+
 function _M:FieldTabComplete()
-  if not AceTab:IsTabCompletionRegistered(tabcompleteName .. "-fields") then
-    AceTab:RegisterTabCompletion(tabcompleteName .. "-fields", " .+",
-      function(t, text, pos)
+  if not AceTab:IsTabCompletionRegistered(self.tabcompleteName .. "-fields") then
+    AceTab:RegisterTabCompletion(self.tabcompleteName .. "-fields", ".+",
+      function(t, text, pos, textToCursor)
+        dbg(textToCursor)
         local lastDot = 0
-        for m in text:gmatch("()%.") do lastDot = m end
+        for m in text:gmatch("()[:.]") do lastDot = m end
         local s = text:find("%s")
-        local m, fs = text:sub(s + 1, lastDot - 1), text:sub(lastDot + 1, -1)
+        local m, dot, fs = text:sub(s + 1, lastDot - 1), text:sub(lastDot, lastDot), text:sub(lastDot + 1, -1)
         fs = fs or ""
+        dbg(m, dot, fs)
         if type(m) == "string" then
-          local tmp = loadstring("return " .. m)()
+          local f = setfenv(loadstring("return " .. m), _G)
+          local tmp = f()
+          print(type(tmp))
           if type(tmp) == "table" then
+            if dot == ":" then
+              tmp = getmetatable(tmp) and getmetatable(tmp).__index or tmp
+            end
+            dbg(tmp)
             for k in pairs(tmp) do
+              local candidate =  m .. dot .. k
+              dbg(candidate)
+              if dot == ":" then
+                if type(setfenv(loadstring("return " .. candidate:gsub(":", ".")), _G)()) ~= "function" then
+                  candidate = nil
+                end
+              end
+              
               if fs:len() > 0 then
-                if k:lower():find(fs, 1, true) == 1 then
-                  t[#t + 1] = m .. "." .. k
+                dbg(k, fs)
+
+                if tostring(k):lower():find(fs:lower(), 1, true) == 1 then
+                  t[#t + 1] = candidate
                 end
               else
-                t[#t + 1] = m .. "." .. k
+                t[#t + 1] = candidate
               end
             end
             return t
           end
         end
       end,
-      nil, -- usagefunc
+      nil,
       nil, -- listenframes
       nil, -- postfunc
       nil) -- pmoverwrite
@@ -91,7 +111,8 @@ end
 
 function _M:GetResultCompletions(u, cands, ...)
   local candcount = 0
-  for k, v in pairs(cands) do candcount = candcount + 1
+  for k, v in pairs(cands) do
+    candcount = candcount + 1
   end
   if candcount <= self.maxResults then
     local text
@@ -102,6 +123,8 @@ function _M:GetResultCompletions(u, cands, ...)
   else
     return "   " .. ("Too many matches (%d possible)"):format(candcount)
   end
+
+  return "error in usage function"
 end
 
 function _M:GetPrefilteredCompletions(t, text, pos)
