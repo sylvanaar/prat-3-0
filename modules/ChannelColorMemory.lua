6a75a313d4d48b189562feb6fd2268bda9b7829b
sylvanaar <sylvanaar@mindspring.com>
2011-06-27 15:33:34 -0400

    code cleanup

--- modules/ChannelColorMemory.lua
+++ modules/ChannelColorMemory.lua
@@ -25,235 +25,204 @@
 -------------------------------------------------------------------------------
 
 
-Prat:AddModuleToLoad(function() 
+Prat:AddModuleToLoad(function()
 
-local PRAT_MODULE = Prat:RequestModuleName("ChannelColorMemory")
+  local PRAT_MODULE = Prat:RequestModuleName("ChannelColorMemory")
 
-if PRAT_MODULE == nil then 
-    return 
-end
+  if PRAT_MODULE == nil then
+    return
+  end
 
-local L = Prat:GetLocalizer({})
+  local L = Prat:GetLocalizer({})
 
---@debug@
-L:AddLocale("enUS", {
+  --@debug@
+  L:AddLocale("enUS", {
     ["ChannelColorMemory"] = true,
     ["Remembers the colors of each channel name."] = true,
     ["(%w+)%s?(.*)"] = "([^%s]+)%s?(.*)",
-})
---@end-debug@
+  })
+  --@end-debug@
 
--- These Localizations are auto-generated. To help with localization
--- please go to http://www.wowace.com/projects/prat-3-0/localization/
-
-
---[===[@non-debug@
-L:AddLocale("enUS", 
---@localization(locale="enUS", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
-)
-L:AddLocale("frFR",  
---@localization(locale="frFR", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
-)
-L:AddLocale("deDE", 
---@localization(locale="deDE", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
-)
-L:AddLocale("koKR",  
---@localization(locale="koKR", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
-)
-L:AddLocale("esMX",  
---@localization(locale="esMX", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
-)
-L:AddLocale("ruRU",  
---@localization(locale="ruRU", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
-)
-L:AddLocale("zhCN",  
---@localization(locale="zhCN", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
-)
-L:AddLocale("esES",  
---@localization(locale="esES", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
-)
-L:AddLocale("zhTW",  
---@localization(locale="zhTW", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
-)
---@end-non-debug@]===]
+  -- These Localizations are auto-generated. To help with localization
+  -- please go to http://www.wowace.com/projects/prat-3-0/localization/
 
 
-local module = Prat:NewModule(PRAT_MODULE, "AceEvent-3.0")
+  --[===[@non-debug@
+  L:AddLocale("enUS",
+  --@localization(locale="enUS", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
+  )
+  L:AddLocale("frFR",
+  --@localization(locale="frFR", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
+  )
+  L:AddLocale("deDE",
+  --@localization(locale="deDE", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
+  )
+  L:AddLocale("koKR",
+  --@localization(locale="koKR", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
+  )
+  L:AddLocale("esMX",
+  --@localization(locale="esMX", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
+  )
+  L:AddLocale("ruRU",
+  --@localization(locale="ruRU", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
+  )
+  L:AddLocale("zhCN",
+  --@localization(locale="zhCN", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
+  )
+  L:AddLocale("esES",
+  --@localization(locale="esES", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
+  )
+  L:AddLocale("zhTW",
+  --@localization(locale="zhTW", format="lua_table", same-key-is-true=true, namespace="ChannelColorMemory")@
+  )
+  --@end-non-debug@]===]
 
-Prat:SetModuleDefaults(module.name, {
-	profile = {
-	    on = true,
-	    colors = {},
-	}
-} )
 
-Prat:SetModuleOptions(module.name, {
-        name = L["ChannelColorMemory"],
-        desc = L["Remembers the colors of each channel name."],
-        type = "group",
-        args = {
-			info = {
-				name = "This module remembers what color you give to a channel with a particular name, so that if you rejoin the channel, no matter what number it is, it will always have the same color.",
-				type = "description",
-			}
-        }
+  local module = Prat:NewModule(PRAT_MODULE, "AceEvent-3.0")
+
+  Prat:SetModuleDefaults(module.name, {
+    profile = {
+      on = true,
+      colors = {},
     }
-)
+  })
 
---[[------------------------------------------------
-    Moduleule Event Functions
-------------------------------------------------]]--
+  Prat:SetModuleOptions(module.name, {
+    name = L["ChannelColorMemory"],
+    desc = L["Remembers the colors of each channel name."],
+    type = "group",
+    args = {
+      info = {
+        name = "This module remembers what color you give to a channel with a particular name, so that if you rejoin the channel, no matter what number it is, it will always have the same color.",
+        type = "description",
+      }
+    }
+  })
 
--- things to do when the moduleule is enabled
-function module:OnModuleEnable()
+  --[[------------------------------------------------
+      Moduleule Event Functions
+  ------------------------------------------------]] --
+
+  -- things to do when the moduleule is enabled
+  function module:OnModuleEnable()
     self:RegisterEvent("UPDATE_CHAT_COLOR")
     self:RegisterEvent("CHAT_MSG_CHANNEL_NOTICE")
-	self.zoneChanIdx = {}
+    self.zoneChanIdx = {}
 
-	-- upgrade saved channel names to lowercase only
-	for k,v in pairs(self.db.profile.colors) do
-		if k ~= k:lower() then
-			self.db.profile.colors[k:lower()] = v
-		end
-	end
-	self:RestoreAllChatColors()
---	self:RememberAllCurrentChannels()
-end
+    -- upgrade saved channel names to lowercase only
+    for k, v in pairs(self.db.profile.colors) do
+      if k ~= k:lower() then
+        self.db.profile.colors[k:lower()] = v
+      end
+    end
+    self:RestoreAllChatColors()
+  end
 
-local function dbg(...)
-   --  Prat:PrintLiteral(...)
-end
+  function module:IndexServerChannels()
+    for k, v in pairs(Prat.HookedFrames) do
+      local t = { GetChatWindowChannels(v:GetID()) }
+      for i = 1, #t, 2 do
+        local chan, num = t[i], t[i + 1]
+        if tonumber(num) and tonumber(num) > 0 then
+          self.zoneChanIdx[tostring(num)] = tostring(chan)
+        end
+      end
+    end
+  end
 
-function module:IndexServerChannels()
-	for k,v in pairs(Prat.HookedFrames) do
-		local t = {GetChatWindowChannels(v:GetID())}
-		for i=1,#t,2 do
-			local chan, num = t[i], t[i+1]
-			if tonumber(num) and tonumber(num) > 0 then
-				self.zoneChanIdx[tostring(num)] = tostring(chan) 
-			end
-		end
-	end
-end
-
-function module:RememberAllCurrentChannels()
-	for k,v in pairs(Prat.HookedFrames) do
-		local t = {GetChatWindowChannels(v:GetID())}
-		for i=1,#t,2 do
-			local chan, num = t[i], t[i+1]
-
-			if chan and chan:len()>0 then
-                local color = self.db.profile.colors[chan:lower()];
-                if (not color) then
-                    self.db.profile.colors[chan:lower()] = {r=cr, g=cg, b=cb};
-                else
-                    color.r=cr
-                    color.g=cg
-                    color.b=cb
-                end
-			end
-		end		
-	end
-end
-
-function module:RestoreAllChatColors()
-	for k,v in pairs(Prat.HookedFrames) do
-		local t = {GetChatWindowChannels(v:GetID())}
-		for i=1,#t,2 do
-			local chan, num = t[i], t[i+1]
-			if tonumber(num) and tonumber(num) > 0 then
-				self.zoneChanIdx[tostring(num)] = tostring(chan) 
-			end
-			if chan and chan:len()>0 then
-                local color = self.db.profile.colors[chan:lower()];
-                if  color then                
-					local number = GetChannelName(chan);
-					if number then
-						--Prat:Print(tostring(number).." "..tostring(chan))
-						ChangeChatColor("CHANNEL"..number, color.r, color.g, color.b);
-					end
-                end
-			end
-		end		
-	end
-end		
-
---[[------------------------------------------------
-    Core Functions
-------------------------------------------------]]--
-
-
-local function getServerChan(name)
-    local t = {EnumerateServerChannels()}
-	for _, channame in pairs(t) do
-		if channame:lower() == name:lower() then
-			return channame
-		end
-	end
-end
-		
-
-function module:UPDATE_CHAT_COLOR(evt, ChatType, cr,cg,cb)
-	dbg(evt, ChatType, cr,cg,cb)
-    if (ChatType) then
-        local number = ChatType:match("CHANNEL(%d+)")
-        if ( number ) then
-            local _, name = GetChannelName(number);
-            if ( name ) then
-                local name, zoneSuffix = name:match(L["(%w+)%s?(.*)"]);
-				if zoneSuffix and zoneSuffix:len() > 0 then
-				local cname = name
-
-					name = getServerChan(name)
-					--Prat:Print("Server Chan @"..tostring(number)..": "..tostring(cname).. " -> "..tostring(name))
-				end
-
-				if not name then return end
-
-                local color = self.db.profile.colors[name:lower()];
-                if (not color) then
-                    self.db.profile.colors[name:lower()] = {r=cr, g=cg, b=cb};
-                else
-                    color.r=cr
-                    color.g=cg
-                    color.b=cb
-                end
+  function module:RestoreAllChatColors()
+    for k, v in pairs(Prat.HookedFrames) do
+      local t = { GetChatWindowChannels(v:GetID()) }
+      for i = 1, #t, 2 do
+        local chan, num = t[i], t[i + 1]
+        if tonumber(num) and tonumber(num) > 0 then
+          self.zoneChanIdx[tostring(num)] = tostring(chan)
+        end
+        if chan and chan:len() > 0 then
+          local color = self.db.profile.colors[chan:lower()];
+          if color then
+            local number = GetChannelName(chan);
+            if number then
+              ChangeChatColor("CHANNEL" .. number, color.r, color.g, color.b);
             end
+          end
         end
+      end
     end
-end
+  end
 
-function module:CHAT_MSG_CHANNEL_NOTICE(evt, NoticeType, Sender, Language, LongName, Target, Flags, ServChanID, number, cname, unknown, counter)
-	dbg(evt, NoticeType, Sender, Language, LongName, Target, Flags, ServChanID, number, cname, unknown, counter)
-	if tonumber(ServChanID) > 0 then 
-		--Prat:Print("Server Chan @"..tostring(number)..": "..ServChanID.."  "..tostring(cname).. " -> "..tostring(self.zoneChanIdx[tostring(ServChanID)]))
-		cname = self.zoneChanIdx[tostring(ServChanID)]
+  --[[------------------------------------------------
+      Core Functions
+  ------------------------------------------------]] --
 
-		if not cname then 
-			self:IndexServerChannels()
-		
-			cname = self.zoneChanIdx[tostring(ServChanID)]
-		--Prat:Print("Server Chan2: "..ServChanID.."  "..tostring(cname).. " -> "..tostring(self.zoneChanIdx[tostring(ServChanID)]))
-		end
-	end
 
-	if number == nil or cname == nil then 
-		return
+  local function getServerChan(name)
+    local t = { EnumerateServerChannels() }
+    for _, channame in pairs(t) do
+      if channame:lower() == name:lower() then
+        return channame
+      end
+    end
+  end
+
+
+  function module:UPDATE_CHAT_COLOR(evt, ChatType, cr, cg, cb)
+    if (ChatType) then
+      local number = ChatType:match("CHANNEL(%d+)")
+      if (number) then
+        local _, name = GetChannelName(number);
+        if (name) then
+          local name, zoneSuffix = name:match(L["(%w+)%s?(.*)"]);
+          if zoneSuffix and zoneSuffix:len() > 0 then
+            local cname = name
+
+            name = getServerChan(name)
+          end
+
+          if not name then return end
+
+          local color = self.db.profile.colors[name:lower()];
+          if (not color) then
+            self.db.profile.colors[name:lower()] = { r = cr, g = cg, b = cb };
+          else
+            color.r = cr
+            color.g = cg
+            color.b = cb
+          end
+        end
+      end
+    end
+  end
+
+  function module:CHAT_MSG_CHANNEL_NOTICE(evt, NoticeType, Sender, Language, LongName, Target, Flags, ServChanID,
+    number, cname, unknown, counter)
+    if tonumber(ServChanID) > 0 then
+      cname = self.zoneChanIdx[tostring(ServChanID)]
+
+      if not cname then
+        self:IndexServerChannels()
+
+        cname = self.zoneChanIdx[tostring(ServChanID)]
+      end
+    end
+
+    if number == nil or cname == nil then
+      return
     elseif (NoticeType == "YOU_JOINED") then
-        local color = self.db.profile.colors[cname:lower()];
-        if (color) then
-            ChangeChatColor("CHANNEL"..number, color.r, color.g, color.b);
-        end
-	elseif (NoticeType == "YOU_LEFT") then
-        local color = self.db.profile.colors[cname:lower()];
-        if (color) then
-            ChangeChatColor("CHANNEL"..number, 1.0, 0.75, 0.75);
-        else
-            color = ChatTypeInfo["CHANNEL"..number];
-            self.db.profile.colors[cname:lower()] = {r=color.r, g=color.g, b=color.b};
-		end
+      local color = self.db.profile.colors[cname:lower()];
+      if (color) then
+        ChangeChatColor("CHANNEL" .. number, color.r, color.g, color.b);
+      end
+    elseif (NoticeType == "YOU_LEFT") then
+      local color = self.db.profile.colors[cname:lower()];
+      if (color) then
+        ChangeChatColor("CHANNEL" .. number, 1.0, 0.75, 0.75);
+      else
+        color = ChatTypeInfo["CHANNEL" .. number];
+        self.db.profile.colors[cname:lower()] = { r = color.r, g = color.g, b = color.b };
+      end
     end
-end
+  end
 
   return
-end ) -- Prat:AddModuleToLoad
+end) -- Prat:AddModuleToLoad
