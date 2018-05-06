6bd25aa86711b93dc08ae7b41d480410b3c199a4
sylvanaar <sylvanaar@mindspring.com>
2018-05-06 08:59:27 -0700

    Added CHAT_MSG_LOOT the the processed events for the sake of scrollback

--- services/events.lua
+++ services/events.lua
@@ -59,6 +59,7 @@ local eventMap = {
   CHAT_MSG_BN_WHISPER = true,
   CHAT_MSG_BN_WHISPER_INFORM = true,
   CHAT_MSG_BN_CONVERSATION = true,
+  CHAT_MSG_LOOT = true
 }
 
 function EnableProcessingForEvent(event, flag)
