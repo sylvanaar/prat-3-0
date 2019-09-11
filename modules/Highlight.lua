b0dff013bc2713e2cdfda5ba4e4ec9eb22d219c4
sylvanaar <sylvanaar@mindspring.com>
2019-09-11 03:12:19 -0700

    Add localization to the Highlight module - it has a purpose now

--- modules/Highlight.lua
+++ modules/Highlight.lua
@@ -68,52 +68,52 @@ Prat:AddModuleToLoad(function()
       local L
 
   
-  --@localization(locale="enUS", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Achievements")@
+  --@localization(locale="enUS", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Highlight")@
   PL:AddLocale(PRAT_MODULE, "enUS", L)
 
 
   
-  --@localization(locale="itIT", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Achievements")@
+  --@localization(locale="itIT", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Highlight")@
   PL:AddLocale(PRAT_MODULE, "itIT", L)
 
 
   
-  --@localization(locale="ptBR", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Achievements")@
+  --@localization(locale="ptBR", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Highlight")@
   PL:AddLocale(PRAT_MODULE, "ptBR", L)
 
 
   
-  --@localization(locale="frFR", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Achievements")@
+  --@localization(locale="frFR", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Highlight")@
   PL:AddLocale(PRAT_MODULE, "frFR", L)
 
 
   
-  --@localization(locale="deDE", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Achievements")@
+  --@localization(locale="deDE", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Highlight")@
   PL:AddLocale(PRAT_MODULE, "deDE", L)
 
 
   
-  --@localization(locale="koKR", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Achievements")@
+  --@localization(locale="koKR", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Highlight")@
   PL:AddLocale(PRAT_MODULE, "koKR",  L)
 
   
-  --@localization(locale="esMX", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Achievements")@
+  --@localization(locale="esMX", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Highlight")@
   PL:AddLocale(PRAT_MODULE, "esMX",  L)
 
   
-  --@localization(locale="ruRU", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Achievements")@
+  --@localization(locale="ruRU", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Highlight")@
   PL:AddLocale(PRAT_MODULE, "ruRU",  L)
 
   
-  --@localization(locale="zhCN", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Achievements")@
+  --@localization(locale="zhCN", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Highlight")@
   PL:AddLocale(PRAT_MODULE, "zhCN",  L)
 
   
-  --@localization(locale="esES", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Achievements")@
+  --@localization(locale="esES", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Highlight")@
   PL:AddLocale(PRAT_MODULE, "esES",  L)
 
   
-  --@localization(locale="zhTW", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Achievements")@
+  --@localization(locale="zhTW", format="lua_table", handle-subnamespaces="none", same-key-is-true=true, namespace="Highlight")@
   PL:AddLocale(PRAT_MODULE, "zhTW",  L)
   end
   --@end-non-debug@]===]
